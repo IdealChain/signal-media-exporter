c07feb6f9ee45407114bb151d3543eeca7f54c04
Daniel Achleitner <daniel.achleitner@gmail.com>
2019-09-20 20:42:27 +0200

    SQLCipher 4: use default settings (Signal Desktop has migrated)

--- export.py
+++ export.py
@@ -14,12 +14,7 @@ from pathlib import Path
 from pysqlcipher3 import dbapi2 as sqlite
 
 logger = logging.getLogger(__name__)
-sqlcipher_settings = {
-    'cipher_page_size': 1024,
-    'kdf_iter': 64000,
-    'cipher_hmac_algorithm': 'HMAC_SHA1',
-    'cipher_kdf_algorithm': 'PBKDF2_HMAC_SHA1'
-}
+sqlcipher_settings = {}
 
 def get_key(config):
     with open(os.path.join(config['signalDir'], 'config.json'), 'r') as f:
